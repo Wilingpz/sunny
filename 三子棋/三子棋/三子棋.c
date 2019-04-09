4d11853460f6c774f0979b14cae49ed3587a50b5
wling_pz <1874078207@qq.com>
2019-04-09 23:24:34 +0800

    三子棋.c

--- 三子棋/三子棋/三子棋.c
+++ 三子棋/三子棋/三子棋.c
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-  
+
 
 int Menu()
 {
@@ -32,7 +32,7 @@ void Print()
 {
 	for (int row = 0; row < MAX_ROW; row++)
 	{
-		printf("| %c | %c | %c |\n", chess_board[row][0], chess_board[row][1],chess_board[row][2]);
+		printf("| %c | %c | %c |\n", chess_board[row][0], chess_board[row][1], chess_board[row][2]);
 		if (row != MAX_ROW - 1)
 		{
 			printf("|---|---|---|\n");
@@ -44,7 +44,7 @@ void PlayerMove()
 {
 	printf("玩家落子\n");
 	while (1)
-  {
+	{
 		printf("请输入一组坐标(row,col): ");
 		int row = 0;
 		int col = 0;
@@ -57,7 +57,7 @@ void PlayerMove()
 		if (chess_board[row][col] != ' ')
 		{
 			printf("您输入的坐标已经被占用\n");
-		continue;
+			continue;
 		}
 		chess_board[row][col] = 'x';
 		break;
@@ -124,7 +124,7 @@ char CheckGameOver()
 	}
 	if (IsFull())
 	{
-		return 'q';		
+		return 'q';
 	}
 	return ' ';
 
@@ -133,43 +133,43 @@ char CheckGameOver()
 
 void Game()
 {
-		Init();
-		char winner = ' ';
-		while (1)
+	Init();
+	char winner = ' ';
+	while (1)
+	{
+		system("cls");
+		Print();
+		PlayerMove();
+		winner = CheckGameOver();
+		if (winner != ' ')
 		{
-			system("cls");
-			Print();
-			PlayerMove();
-			winner = CheckGameOver();
-			if (winner != ' ')
-			{
-				break;
-			}
-			ComputerMove();
-			winner = CheckGameOver();
-			if (winner != ' ')
-			{
-				break;
-			}
+			break;
 		}
-		  if (winner == 'x')
-		  {
-			  printf("玩家胜利!\n");
-		  }
-		  else if (winner = 'o')
-		  {
-			  printf("你真菜，电脑都下不过!\n");
-		  }
-		  else if (winner = 'q')
-		  {
-			  printf("你还是真菜，和电脑五五开!\n");
-		  }
-		  else
-		  {
-			  printf("好像出错了!\n");
-		  }
-		  Print();
-  }
+		ComputerMove();
+		winner = CheckGameOver();
+		if (winner != ' ')
+		{
+			break;
+		}
+	}
+	if (winner == 'x')
+	{
+		printf("玩家胜利!\n");
+	}
+	else if (winner = 'o')
+	{
+		printf("你真菜，电脑都下不过!\n");
+	}
+	else if (winner = 'q')
+	{
+		printf("你还是真菜，和电脑五五开!\n");
+	}
+	else
+	{
+		printf("好像出错了!\n");
+	}
+	Print();
+}
 
 int main()
 {
@@ -190,6 +190,6 @@ int main()
 			printf("您的输入有误！\n");
 		}
 	}
-		system("pause");
-		return 0;
+	system("pause");
+	return 0;
 }

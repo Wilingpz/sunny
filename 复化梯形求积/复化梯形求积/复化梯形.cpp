ce433960ef05ce556ab4b0fcff1d26d809890434
wling_pz <1874078207@qq.com>
2020-01-12 13:19:14 +0800

    复化梯形求积公式

---
+++ 复化梯形求积/复化梯形求积/复化梯形.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+//保存生成的节点横坐标
+double dataX[1000];
+//保存生成的节点纵坐标
+double dataY[1000];
+//定义一个数学函数
+double function(double x);
+//分割区间
+void devide(double a, double b, int N);
+//复化梯形求积
+double getIntegralValue(double a, double b, int N);
+
+int main()
+{
+	//产生的节点数
+	double a, b;
+	int N;
+	char ch = 'n';
+	while (ch != 'y')
+	{
+		cout << "请输入等分区间数：";
+		cin >> N;
+		cout << "请输入左右区间范围：";
+		cin >> a;
+		cin >> b;
+		devide(a, b, N);//调用分割区间函数
+		cout << "积分值为：" << getIntegralValue(a, b, N) << endl;
+		//调用复化求积公式
+	}
+	return 0;
+}
+//定义一个数学函数
+double function(double x)
+{
+	return 4 / (1 + x*x);
+}
+//分割区间
+void devide(double a, double b, int N)
+{
+	double x;
+	double dX = (b - a) / (N);
+	for (int i = 0; i <= N; i++)
+	{
+		x = a + i * dX;
+		dataX[i] = x;
+		dataY[i] = function(x);//存储分割段的函数自变量及函数值
+	}
+}
+//复化梯形求积
+double getIntegralValue(double a, double b, int N)
+{
+	double sum = 0;
+	//求2*f(xk)的总和，1<= k <= n-1
+	for (int i = 1; i <= N - 1; i++)
+	{
+		sum += 2 * dataY[i];
+	}
+	double value = 0.5* ((b - a) / N)*(dataY[0] + sum + dataY[N]);//公式
+	return value;
+}

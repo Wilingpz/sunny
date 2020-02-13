aa857d09c1fefdb6a912c9011cbf8432610552b5
wling_pz <1874078207@qq.com>
2020-02-13 09:14:23 +0800

    2020.模拟

---
+++ 2020.8/2020.8/main1cpp.cpp
@@ -0,0 +1,56 @@
+﻿//给定一个整数类型的数组 nums，请编写一个能够返回数组“中心索引”的方法。
+//
+//我们是这样定义数组中心索引的：
+//数组中心索引的左侧所有元素相加的和等于右侧所有元素相加的和。
+//
+//如果数组不存在中心索引，那么我们应该返回 - 1。
+//如果数组有多个中心索引，那么我们应该返回最靠近左边的那一个。
+//
+//示例 1:
+//
+//输入 :
+//	nums = [1, 7, 3, 6, 5, 6]
+//输出 : 3
+// 解释 :
+//	索引3(nums[3] = 6) 的左侧数之和(1 + 7 + 3 = 11)，与右侧数之和(5 + 6 = 11)相等。
+//	同时, 3 也是第一个符合要求的中心索引
+//
+//	来源：力扣（LeetCode）
+//链接：https ://leetcode-cn.com/problems/find-pivot-index
+//   著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution 
+{
+public:
+	int pivotIndex(vector<int>& nums) 
+	{
+		if ((nums.size() == 0)||(nums.size()==2))
+		{
+			return -1;
+		}
+		if (nums.size() == 1)
+		{
+			return nums[0];
+		}
+		int sum = 0;
+		int tmp = 0;
+		for (auto &i : nums)
+		{
+			sum += i;
+		}
+		for (int i = 0; i < nums.size(); i++)
+		{
+			sum -=  nums[i];
+			if (tmp == sum)
+			{
+				return i;
+			}
+			tmp += nums[i];
+		}
+		return -1;
+	}
+};
+
+
